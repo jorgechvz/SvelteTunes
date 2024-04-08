ea7aa9b4e5bdc9dfe64d6857de73450b72b3c88f
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:28:26 -0500

    Add genre API server file

---
+++ src/routes/api/genre/[id]/+server.js
@@ -0,0 +1,12 @@
+import { json } from "@sveltejs/kit";
+
+export async function GET({ params }) {
+  const response = await fetch(
+    `https://api.deezer.com/genre/${params.id}`
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const result = await response.json();
+  return json(result);
+}
