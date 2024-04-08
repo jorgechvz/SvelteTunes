c51e2fed179d1d50fede2d6e926c6b42b9585557
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:27:42 -0500

    Add hero playlist component

---
+++ src/lib/components/ui/playlists/hero-playlist.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let playlist
+    const convertTime = (time) => {
+        const hours = Math.floor(time / 3600)
+        const minutes = Math.floor((time % 3600) / 60)
+        return `${hours}h. ${minutes} min.`
+    }
+</script>
+
+<section>
+    <div class="flex gap-10 items-center">
+        <img class="w-full max-w-xs" src={playlist.picture_big} alt='image for{playlist.title}'>
+        <div>
+            <h1 class="text-7xl font-bold mb-10">{playlist.title}</h1>
+            <h3>{playlist.creator.name}</h3>
+            <div class="flex gap-2 text-gray-500 mt-2">
+                <p>{playlist.nb_tracks} songs</p>
+                <p>|</p>
+                <p>{convertTime(playlist.duration)}</p>
+                <p>|</p>
+                <p>{playlist.fans} fans</p>
+            </div>
+            <p class="mt-10 text-gray-500">{playlist.description}</p>
+        </div>
+    </div>
+</section>
