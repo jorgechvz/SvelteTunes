a443440100e6d92cc5d666d838dcdb6154ff2488
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:25:00 -0500

    Add artists charts component

---
+++ src/lib/components/ui/artists/artists-charts.svelte
@@ -0,0 +1,36 @@
+<script>
+  import * as Carousel from "$lib/components/ui/carousel/index";
+  import * as Card from "$lib/components/ui/card/index";
+  export let artists;
+</script>
+
+<section class="max-w-7xl mx-auto">
+  <Carousel.Root class="w-full max-w-7xl">
+    <Carousel.Content class="-ml-1">
+      {#each artists as artist}
+        <Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/5">
+          <div class="p-1">
+            <Card.Root class="border-[0px] shadow-[0px]">
+              <Card.Content
+                class="flex aspect-square items-center justify-center p-1"
+              >
+                <a href="/artist/{artist.id}">
+                  <img
+                    class="w-full rounded-full"
+                    src={artist.picture_medium}
+                    alt={artist.name}
+                  />
+                  <div class="mt-2">
+                    <h3 class="font-bold text-center">{artist.name}</h3>
+                  </div>
+                </a>
+              </Card.Content>
+            </Card.Root>
+          </div>
+        </Carousel.Item>
+      {/each}
+    </Carousel.Content>
+    <Carousel.Previous />
+    <Carousel.Next />
+  </Carousel.Root>
+</section>
