5e617541625737730cf1a4fc0c30a3503f1d0a70
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:27:50 -0500

    Add playlist player music component

---
+++ src/lib/components/ui/playlists/playlist-player-music.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Input from "$lib/components/ui/input/input.svelte";
+  import FaSearch from "svelte-icons/fa/FaSearch.svelte";
+  import { search } from "$lib/stores/store.js";
+</script>
+
+<section class="mt-10 flex items-center justify-between">
+  <h3 class="text-2xl font-bold">Playlist's Tracks</h3>
+  <form action="" class="max-w-sm w-full">
+    <div class="relative">
+      <div class="absolute left-3 top-3 h-4 w-4 text-muted-foreground">
+        <FaSearch />
+      </div>
+      <Input
+        bind:value={$search}
+        class="pl-10 bg-gray-200"
+        type="text"
+        placeholder="Search within song"
+      />
+    </div>
+  </form>
+</section>
