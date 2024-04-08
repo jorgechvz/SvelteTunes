17e25c889d6b00d456a326db10ae5e898fbfdc12
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:28:00 -0500

    Add store.js file with initial track list and store variables

---
+++ src/lib/stores/store.js
@@ -0,0 +1,24 @@
+import { writable, get } from "svelte/store";
+
+export const audioPlayer = writable();
+export const status = writable("default");
+export const isPlaying = writable(false);
+export const index = writable(0);
+export const search = writable(''); 
+
+export const trackList = writable([
+  {
+		title: 'Demons',
+		artist: 'Imagine Dragons',
+		file: 'https://cdns-preview-f.dzcdn.net/stream/c-f69a51d64ef9368d16bb9c880ab4a949-6.mp3',
+		image: 'https://e-cdns-images.dzcdn.net/images/cover/7e8314f4280cffde363547a495a260bc/56x56-000000-80-0-0.jpg'
+	},
+]);
+
+export const addTrack = (track) => {
+  trackList.update((v) => [...v, track]);
+};
+
+export const setTrack = (trackIndex) => {
+  index.set(trackIndex);
+};
