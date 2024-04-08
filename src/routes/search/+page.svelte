ae3e0f25807c13a39cb4dae0038225ed7bea75a6
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:28:56 -0500

    Add search page component

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let data;
+  $: ({ results } = data);
+</script>
+
+<div class="max-w-7xl mx-auto">
+  <ul>
+    {#each results.data as result}
+      <li class="px-4 py-2 cursor-pointer">{result.title}</li>
+    {/each}
+  </ul>
+</div>
