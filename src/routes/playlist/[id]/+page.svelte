00aa90e910cce3c6a65391ad313d50f36de73626
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:28:49 -0500

    Add playlist page component

---
+++ src/routes/playlist/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import HeroPlaylist from "$lib/components/ui/playlists/hero-playlist.svelte";
+  import PlaylistPlayerMusic from "$lib/components/ui/playlists/playlist-player-music.svelte";
+  import TracksTable from "$lib/components/ui/playlists/tracks-table.svelte";
+  export let data;
+  $: ({ playlist } = data);
+</script>
+
+<section class="max-w-7xl mx-auto pt-10 pb-32">
+  <HeroPlaylist {playlist} />
+  <PlaylistPlayerMusic />
+  <TracksTable {playlist} />
+</section>
