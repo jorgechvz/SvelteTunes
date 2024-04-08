dfc48d161edc9b89ac9ba388ae6e4974f1dbe9ef
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 15:28:04 -0500

    Add layout component with sidebar, search bar, and player

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+  import "../app.pcss";
+  import Player from "$lib/components/ui/commons/player.svelte";
+  import Sidebar from "$lib/components/ui/commons/sidebar.svelte";
+  import SearchBar from "$lib/components/ui/commons/search-bar.svelte";
+</script>
+
+<div class="flex flex-row">
+  <div class="w-1/6 px-5">
+    <div class="fixed border-r-2 h-screen">
+      <Sidebar />
+    </div>
+  </div>
+  <div class="w-11/12 py-2">
+    <div class="mb-32">
+      <SearchBar />
+    </div>
+      <slot />
+  </div>
+</div>
+<div c8lass="w-full">
+  <Player />
+</div>
+
+<style>
+  ::-webkit-scrollbar {
+    background: transparent; /* make scrollbar transparent */
+  }
+
+  :global(html) {
+    scrollbar-width: none; /* For Firefox */
+    -ms-overflow-style: none; /* For Internet Explorer and Edge */
+  }
+</style>
