0751f9830c25a5f7e51d3c745565b192f1c26fe5
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 20:21:54 -0500

    Add playlist search API endpoint

---
+++ src/routes/api/search/playlist/+server.js
@@ -0,0 +1,13 @@
+import { json } from "@sveltejs/kit";
+
+export async function GET(event) {
+  const { searchParams } = event.url;
+  const search = searchParams.get("q");
+  const index = searchParams.get("index") || 0; // Get the index parameter, default to 0 if not provided
+  const response = await fetch(`https://api.deezer.com/search/playlist?q=${search}&index=${index}`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const result = await response.json();
+  return json(result);
+}
