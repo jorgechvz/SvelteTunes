45842c05078c9bca49995072c101550610a0bb3d
jorgechvz <jorgechvzpon@gmail.com>
2024-04-08 20:21:46 -0500

    Update search API endpoint to include index parameter

--- src/routes/api/search/+server.js
+++ src/routes/api/search/+server.js
@@ -3,7 +3,10 @@ import { json } from "@sveltejs/kit";
 export async function GET(event) {
   const { searchParams } = event.url;
   const search = searchParams.get("q");
-  const response = await fetch(`https://api.deezer.com/search?q=${search}`);
+  const index = searchParams.get("index") || 0; // Get the index parameter, default to 0 if not provided
+  const response = await fetch(
+    `https://api.deezer.com/search/track?q=${search}&index=${index}`
+  );
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
